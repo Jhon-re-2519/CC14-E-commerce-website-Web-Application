0c01e03cb3173152f27a1e7b2986595d0ae6247d
Jhon-rey Bocoboc <172336502+Jhon-re-2519@users.noreply.github.com>
2025-01-30 19:51:01 +0800

    Lines Added
    typing effect

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ if (close) {
 
 
 // Customer Support Chat
-// Open the chat box
+// Open chat box
 document.querySelector('.chatBtn').addEventListener('click', function() {
     document.querySelector('.chatBox').style.display = 'flex';
 });
 
-// Close the chat box
+// Close chat box
 document.querySelector('.closeChat').addEventListener('click', function() {
     document.querySelector('.chatBox').style.display = 'none';
 });
@@ -30,11 +30,23 @@ document.querySelector('.closeChat').addEventListener('click', function() {
 // Handle message sending
 document.querySelector('.sendBtn').addEventListener('click', function() {
     var message = document.querySelector('.messageInput').value;
-    if (message.trim() !== '') {
+    if (message.trim() !== 'hello') {
         var messageContainer = document.createElement('div');
         messageContainer.innerHTML = `<strong>You:</strong> ${message}`;
         document.querySelector('.chatContent').appendChild(messageContainer);
         document.querySelector('.messageInput').value = ''; // Clear the input field
         document.querySelector('.chatContent').scrollTop = document.querySelector('.chatContent').scrollHeight; // Scroll to the bottom
     }
+});
+
+
+// typing text effect 
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed(".typing-text", {
+        strings: ["Shoes!", "Polos!", "Jackets!", "T-shirts!", "Hats!"],
+        loop: true,
+        typeSpeed: 60,
+        backSpeed: 30,
+        backDelay: 500,
+    });
 });
