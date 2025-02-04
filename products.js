36bbe04ed18693a61d52a9b6da9c31a13aa4fb2c
Jhon-rey Bocoboc <172336502+Jhon-re-2519@users.noreply.github.com>
2025-02-04 21:47:35 +0800

    Lines Fixed
    png to jpg

--- products.js
+++ products.js
@@ -1,169 +1,33 @@
-// Shoes.js
 const shoes = [
-    {
-        id: 1,
-        name: "SAMBA OG SHOES",
-        brand: "adidas",
-        price: 116.40,
-        image: "images/Shoes/Samba green.png", 
-        rating: 5,
-        description: "Long before the adidas Samba shoes took to the streets, they left their mark on the indoor football pitch. Their smooth leather upper and reinforced rubber gum sole were designed with agility and control in mind. Though times have changed, their essence remains. Today, they carry on the legacy as an iconic street style, ready for whatever your day brings. Moulded into their design is decades of history, keeping the spirit of the Trefoil alive.",
+    { id: 1, name: "SAMBA OG SHOES", brand: "adidas", category: "Sneakers", price: 116.40, image: "images/Shoes/Samba green.jpg", rating: 5 },
+    { id: 2, name: "TAEKWONDO LACE SHOES", brand: "adidas", category: "Sneakers", price: 110, image: "images/Shoes/Taekwondo Lace Shoes.jpg", rating: 4 },
+    { id: 3, name: "RETROPY F2 SHOES", brand: "adidas", category: "Sneakers", price: 95, image: "images/Shoes/Retropy F2 Shoes.jpg", rating: 4 },
+    { id: 4, name: "ADIZERO ADIOS PRO 4 SHOES", brand: "adidas", category: "Sneakers", price: 225.52, image: "images/Shoes/Adizero Adios Pro 4 Shoes.jpg", rating: 5 },
+    { id: 5, name: "REEBOK CLASSIC LEATHER", brand: "Reebok", category: "Sneakers", price: 69.38, image: "images/Shoes/reebokclassicleather.jpg", rating: 5 },
+    { id: 6, name: "REEBOOK PHASE COURT", brand: "Reebok", category: "Sneakers", price: 37.61, image: "images/Shoes/reebokphasecourt.jpg", rating: 5 },
+    { id: 7, name: "BB 4000 II", brand: "Reebok", category: "Sneakers", price: 76.95, image: "images/Shoes/bb4000II.jpg", rating: 5 },
+    { id: 8, name: "FLUXLITE", brand: "Reebok", category: "Sneakers", price: 31.09, image: "images/Shoes/fluxlite.jpg", rating: 5 },
+    { id: 9, name: "T500", brand: "New Balance", category: "Sneakers", price: 109.99, image: "images/Shoes/t500.jpg", rating: 5 },
+    { id: 10, name: "574 CORE", brand: "New Balance", category: "Sneakers", price: 89.99, image: "images/Shoes/574core.jpg", rating: 5 },
+    { id: 11, name: "1906 UTILITY", brand: "New Balance", category: "Sneakers", price: 169.99, image: "images/Shoes/1906utility.jpg", rating: 5 },
+    { id: 12, name: "1906 W", brand: "New Balance", category: "Sneakers", price: 154.99, image: "images/Shoes/1906w.jpg", rating: 5 },
+    { id: 13, name: "NIKE DUNK LOW", brand: "Nike", category: "Sneakers", price: 52.98, image: "images/Shoes/nikedunklow.jpg", rating: 5 },
+    { id: 14, name: "NIKE AIR MAX DN PREMIUM", brand: "Nike", category: "Sneakers", price: 160.82, image: "images/Shoes/nikeairmaxdnpremium.jpg", rating: 5 },
+    { id: 15, name: "NIKE AIR MORE UPTEMPO LOW", brand: "Nike", category: "Sneakers", price: 160.82, image: "images/Shoes/nikeairmoreuptempolow.jpg", rating: 5 },
+]
 
-    },
-    {
-        id: 2,
-        name: "TAEKWONDO LACE SHOES",
-        brand: "adidas",
-        price: 110,
-        image: "images/Shoes/Taekwondo Lace Shoes.png",
-        rating: 4,
-        description: "The adidas Taekwondo shoes offer a nod to the early '00s with a minimal, slip-on design that's deeply rooted in the history of martial arts. Crafted from soft leather, they embrace your foot in streamlined comfort, while the super low-profile rubber outsole provides flexible traction. An instant, everyday staple, their unique forefoot stitching, bold 3-Stripes and lace cover give them identity that's all their own.",
-    },
-    {
-        id: 3,
-        name: "RETROPY F2 SHOES",
-        brand: "adidas",
-        price: 95,
-        image: "images/Shoes/Retropy F2 Shoes.png",
-        rating: 4,
-        description: "Life moves fast, but your style looks even faster. Rooted in our running past, these adidas trainers bring looks from '80s track shoes into the present. You can choose to move fast or slow — either way, the lightweight EVA midsole keeps feet feeling comfortable. Show off a material-mix upper of textile, suede and mesh wherever your day leads.",
-    },
-    {
-        id: 4,
-        name: "ADIZERO ADIOS PRO 4 SHOES",
-        brand: "adidas",
-        price: 225.52,
-        image: "images/Shoes/Adizero Adios Pro 4 Shoes.png",
-        rating: 5,
-        description: "Proven to be the world's most winning shoe, the Adizero Adios Pro line is the pinnacle of Adizero Racing shoes. The Adios Pro 4 is made for fast runners who want to experience faster, with enhanced features designed to optimize running efficiency. Our carbon-infused ENERGYRODS 2.0 provide a seamless transition from heel to toe for a snappy, efficient stride. In the midsole, a new rocker point offers improved running economy. A double layer of our best-in-class ultralight LIGHTSTRIKE PRO ensures cushioning for every quick stride you take, helping you maintain energy over the long term. The shoe rides on an outsole built with Continental™ rubber for a no-slip transition during toe-off, as well as LIGHTTRAXION, designed to reduce weight without sacrificing grip.",
-    },
-    {
-        id: 5,
-        name: "REEBOK CLASSIC LEATHER",
-        brand: "Reebook",
-        price: 69.38,
-        image: "images/Shoes/reebokclassicleather.png",
-        rating: 5,
-        description: "Retro-inspired shoes with a Union Jack logo Clean, minimalist design keeps your look forever fresh. These Reebok Classic Leather Shoes update the iconic retro style. A supple leather upper and logos pulled from the archives give you trainers that always work, no matter what you pair them with.",
+const clothing = [
+    { id: 16, name: "CLOTHES", brand: "Nike", category: "Sneakers", price: 135.14, image: "images/Shoes/Book 1.jpg", rating: 5 }
+]
 
-    },
-    {
-        id: 6,
-        name: "REEBOOK PHASE COURT",
-        brand: "Reebook",
-        price: 37.61,
-        image: "images/Shoes/reebokphasecourt.png",
-        rating: 5,
-        description: "Serve up retro court style. These low-cut Reebok shoes are inspired by late 80s Reebok tennis style. This modern-day version features a leather and suede upper. The outsole adds to the authentic vibe.",
-    },
-    {
-        id: 7,
-        name: "BB 4000 II",
-        brand: "Reebook",
-        price: 76.95,
-        image: "images/Shoes/bb4000II.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 8,
-        name: "FLUXLITE",
-        brand: "Reebook",
-        price: 31.09,
-        image: "images/Shoes/fluxlite.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 9,
-        name: "T500",
-        brand: "New Balance",
-        price: 109.99,
-        image: "images/Shoes/t500.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 10,
-        name: "574 CORE",
-        brand: "New Balance",
-        price: 89.99,
-        image: "images/Shoes/574core.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 11,
-        name: "1906 UTILITY",
-        brand: "New Balance",
-        price: 169.99,
-        image: "images/Shoes/1906utility.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 12,
-        name: "1906 W",
-        brand: "New Balance",
-        price: 154.99,
-        image: "images/Shoes/1906w.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 13,
-        name: "NIKE DUNK LOW",
-        brand: "Nike",
-        price: 52.98,
-        image: "images/Shoes/nikedunklow.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 14,
-        name: "NIKE AIR MAX DN PREMIUM",
-        brand: "Nike",
-        price: 160.82,
-        image: "images/Shoes/nikeairmaxdnpremium.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 15,
-        name: "NIKE AIR MORE UPTEMPO LOW",
-        brand: "Nike",
-        price: 160.82,
-        image: "images/Shoes/nikeairmoreuptempolow.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 16,
-        name: "BOOK 1 'FLAGSTAFF' EP",
-        brand: "Nike",
-        price: 135.14,
-        image: "images/Shoes/Book 1.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 17,
-        name: "BOOK 1 'FLAGSTAFF' EP",
-        brand: "Nike",
-        price: 135.14,
-        image: "images/Shoes/Book 1.png",
-        rating: 5,
-        description: "#",
-    },
-    {
-        id: 18,
-        name: "Air Jordan 1 Low Travis Scott x Fragment",
-        brand: "Nike",
-        price: 138.19,
-        image: "images/Shoes/air-jordan-1-low-travis-scott-x-fragment-release-date.jpg",
-        rating: 5,
-        description: "#",
-    },
-        
+const fragrances = [
+    { id: 17, name: "FRAGRANCE", brand: "Nike", category: "Sneakers", price: 135.14, image: "images/Shoes/Book 1.jpg", rating: 5 }
+]
 
-];
+const watches = [
+    { id: 18, name: "WATCHES", brand: "Nike", category: "Sneakers", price: 135.14, image: "images/Shoes/Book 1.jpg", rating: 5 }
+]
+
+const accessories = [
+    { id: 19, name: "ACCCESSORIES", brand: "Nike", category: "Sneakers", price: 135.14, image: "images/Shoes/Book 1.jpg", rating: 5 }
+]
